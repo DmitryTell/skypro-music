0906755809ae17e97a21a9e345e8d3121a910710
DmitryTell <dmitrij.teleganov@gmail.com>
2023-10-16 19:29:43 +0300

    Project started

---
+++ dist/App.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+function App() {
+  return /*#__PURE__*/React.createElement("div", {
+    className: "App"
+  });
+}
+var _default = exports.default = App;
