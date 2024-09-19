8e5e8cc8d818efbce1cdb1515e99ec73a666186c
DmitryTell <dmitrij.teleganov@gmail.com>
2024-09-19 15:05:33 +0200

    project/add/types

---
+++ src/types/button.variants.ts
@@ -0,0 +1,5 @@
+export enum ButtonVariants {
+    BASE = "base",
+    GHOST = "ghost",
+    FILTER = "filter",
+}
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export * from "./button.types";
+export * from "./button.variants";
+export * from "./input.types";
+export * from "./navigation.routes";
+export * from "./theme.types";
