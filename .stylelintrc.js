77f7a92b4d92a91a10821e08198c7597e6786701
DmitryTell <dmitrij.teleganov@gmail.com>
2024-09-19 14:47:47 +0200

    project/workenvironment

---
+++ .stylelintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    processors: ['stylelint-processor-styled-components'],
+    plugins: ['stylelint-order', 'stylelint-prettier'],
+    extends: [
+      'stylelint-config-recommended',
+      'stylelint-config-standard-scss',
+      'stylelint-config-recess-order',
+    ],
+  
+    rules: {
+      'prettier/prettier': true,
+    },
+    configBasedir: __dirname,
+};
